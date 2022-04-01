3e183649bfe1c444a32ac2323aeddee6ac237ab3
Chelsea Cole <73906280+CcoleCode@users.noreply.github.com>
2022-04-01 11:41:07 -0400

    new app

---
+++ src/FirstBank.java
@@ -0,0 +1,14 @@
+
+public class FirstBank {
+
+	public static void main(String[] args) {
+		
+		Account chelsea = new Account("Chelsea Cole", "A000001");
+		chelsea.showMenu();
+		
+		Account tonya = new Account("Tonya Smith", "A000123");
+		tonya.showMenu();
+
+	}
+
+}
